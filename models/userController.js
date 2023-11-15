6d8ea0ec59cba38756255291ee9ac42d74ba5aba
Roshan <roshan.jha@hih7.in>
2023-11-15 12:36:16 +0530

    password has been hashed

--- models/userController.js
+++ models/userController.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,4 +18,16 @@ const userSchema = new mongoose.Schema(
   { versionKey: false }
 );
 
-module.exports = mongoose.model("users", userSchema);
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}),
+  (module.exports = mongoose.model("users", userSchema));
