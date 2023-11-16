a45b6eaeb94f563c47e6601888467ada1ee2775b
Roshan <roshan.jha@hih7.in>
2023-11-16 12:57:28 +0530

    verify access token

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const authRouter = require("./routers/authRoute");
 const connectDB = require("./config/dbConfig");
+const { verifyAccessToken } = require("./utils/jwt_utils");
 
 const app = express();
 connectDB();
@@ -13,7 +14,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res, next) => {
+app.get("/", verifyAccessToken, async (req, res, next) => {
   res.send("hello from express");
 });
 app.use("/auth", authRouter);
--- utils/jwt_utils.js
+++ utils/jwt_utils.js
@@ -7,7 +7,7 @@ module.exports = {
       const payload = {};
       const secret = process.env.JWT_ACCESS_SECRET;
       const options = {
-        expiresIn: "1h",
+        expiresIn: "15s",
         issuer: "sitename.com",
         audience: userId,
       };
@@ -18,6 +18,22 @@ module.exports = {
         }
         resolve(token);
       });
+    });
+  },
+  verifyAccessToken: (req, res, next) => {
+    //if auth header present inside the req
+    if (!req.headers["authorization"]) return next(createError.Unauthorized());
+
+    const authHeader = req.headers["authorization"];
+    //store authHeader inside an array //authheader is token+bearer
+    const bearerToken = authHeader.split(" ");
+    const token = bearerToken[1];
+    jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, payload) => {
+      if (err) {
+        return next(createError.Unauthorized());
+      }
+      req.payload = payload;
+      next();
     });
   },
 };
